5b6b22f2e2cd820c15fc86454845c9ebff28953c
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2020-11-07 20:36:27 +0100

    Working draft for app

--- src/client.py
+++ src/client.py
@@ -1,14 +1,26 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-from dataset import extract_urls
+from dataset import extract_section_to_urls
 from dataset import load_topics
 
 
+@st.cache
+def load_data(path_to_folder: str):
+    all_topics = load_topics(path_to_folder)
+    section_to_urls = extract_section_to_urls(all_topics)
+    return section_to_urls
+
+
 def main():
-    st.title("Parsing Streamlit Weekly Roundups")
-    all_topics = load_topics("./data")
-    urls_to_title = extract_urls(all_topics)
+    st.title("Streamlit Weekly Roundups Explorer")
+    st.sidebar.subheader("Configuration")
+    section_to_urls = load_data("./data")
+
+    selected_section = st.sidebar.selectbox(
+        "Select section: ", sorted(section_to_urls.keys())
+    )
+    urls_to_title = section_to_urls[selected_section]
 
     selected_url = st.selectbox(
         "Search for a link:",
@@ -16,6 +28,7 @@ def main():
         format_func=lambda option: urls_to_title[option],
     )
     st.markdown(f"URL: {selected_url}")
+    st.markdown("Embedded page below. If it does not work, click on the URL above")
     components.iframe(selected_url, height=1200, scrolling=True)
 
 
--- src/dataset.py
+++ src/dataset.py
@@ -1,10 +1,11 @@
-import itertools
+from functools import reduce
 
 from pathlib import Path
 from typing import Dict
 from typing import List
 from typing import Optional
 
+from bs4 import BeautifulSoup
 from pydantic import AnyHttpUrl
 from pydantic import BaseModel
 
@@ -35,14 +36,54 @@ class Topic(BaseModel):
     details: Details
 
 
+def _merge_dict_of_dicts(
+    dict1: Dict[str, Dict], dict2: Dict[str, Dict]
+) -> Dict[str, Dict]:
+    dict3 = {**dict1, **dict2}
+    for key, value in dict3.items():
+        if key in dict1 and key in dict2:
+            dict3[key] = {**value, **dict1[key]}
+    return dict3
+
+
 def load_topics(path_to_folder: str) -> List[Topic]:
+    """Parse list of topics as JSON files from Discourse API"""
     all_files = Path(path_to_folder).rglob("*.json")
     parsed_files = [Topic.parse_file(file) for file in all_files]
     return parsed_files
 
 
-def extract_urls(topics: List[Topic]) -> Dict[str, str]:
-    all_links = itertools.chain.from_iterable(
-        [topic.details.links for topic in topics]
-    )
-    return {l.url: l.title for l in all_links if l.title is not None}
+def extract_section_to_urls(topics: List[Topic]) -> Dict[str, Dict]:
+    """Create Dict of topic section to mapping of URL to titles, like:
+
+    {
+        "Streamlit Updates": {
+            "https://www.streamlit.io/sharing":"Streamlit sharing was announced today",
+            "https://share.streamlit.io/streamlit/demo-uber-nyc-pickups/":"Check out the new Streamlit Sharing"
+        },
+        "Articles": {
+            ...
+        },
+        ...
+    }
+    """
+    parsed_html_per_topic = [
+        BeautifulSoup(topic.post_stream.posts[0].cooked, "html.parser")
+        for topic in topics
+    ]
+
+    section_to_links_per_topic = [
+        {
+            h2_section.text.strip(): {
+                li.find("a")["href"]: li.text.strip()
+                for li in h2_section.find_next_sibling("ul").find_all("li")
+                if li.find("a") is not None
+            }
+            for h2_section in topic_html.find_all("h2")
+        }
+        for topic_html in parsed_html_per_topic
+    ]
+
+    section_to_links = reduce(_merge_dict_of_dicts, section_to_links_per_topic)
+
+    return section_to_links
